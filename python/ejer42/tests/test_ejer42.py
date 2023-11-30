869436b6dde6897e8067ded47bfc4295a2148a0e
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2023-11-30 22:10:31 +0100

    Terminado ejercicio 42 con test unitarios

--- python/ejer42/tests/test_ejer42.py
+++ python/ejer42/tests/test_ejer42.py
@@ -1,4 +1,4 @@
-from ..ejer42 import iva_21, iva_4, discount_10, total
+from ejer42 import iva_21, iva_4, discount_10, total
 
 list_b = [
     {"price": float(100),
@@ -18,7 +18,7 @@ list_b = [
 
 
 def test_iva_21():
-    assert 121 == iva_21(100)
+    assert 121 == iva_21(100) + list_b[0]['price']
 
 
 def test_iva_21b():
@@ -33,10 +33,12 @@ def test_iva_4():
 
 def test_discount():
     global list_b
-    assert 90 == list_b[2]["price"] + list_b[2]["discount"](list_b[2]["price"])
+    assert 90 == list_b[2]["price"] - list_b[2]["discount"](list_b[2]["price"])
 
 
 def test_total():
     global list_b
-    assert {'price': 121.0, 'iva': 21.0, 'discount': 0} == {'price': 121.0, 'iva': 21.0, 'discount': 0}
+    assert {'price': 121.0, 'iva': 21.0, 'discount': 0} == total([list_b][0])
+
+
 
