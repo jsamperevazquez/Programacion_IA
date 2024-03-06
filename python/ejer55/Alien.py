20fa1347dbca877c1023e747376b969739055dcf
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2024-03-06 18:30:59 +0100

    (ejer55): Implementada clase Alien y métodos. Falta tests

--- python/ejer55/Alien.py
+++ python/ejer55/Alien.py
@@ -1,27 +1,58 @@
-class Alien:
-    x: list
-    y: list
-    salud: int
+from typing import Tuple
 
-    def __init__(self, x, y):
+
+class Alien:
+    x: int
+    y: int
+    health: int
+    alien_count: int = 0
+
+    def __init__(self, x, y, health=3):
         self.x = x
         self.y = y
-        self.salud = 3
+        self.health = health
+        Alien.alien_count += 1
 
-    def hit(self, salud_descontar=1):
-        self.salud -= salud_descontar if salud_descontar != 1 else 1
+    def hit(self, discount_health=1) -> int:
+        self.health -= discount_health if discount_health != 1 else 1
+        if self.health <= 0:
+            raise ValueError()
+        return self.health
 
-    def is_alive(self):
-        return True if self.salud > 0 else False
+    def is_alive(self) -> bool:
+        return True if self.health > 0 else False
 
-    def teleporting(self, x_teleport: list, y_teleport: list):
+    def teleporting(self, x_teleport: int, y_teleport: int) -> tuple[int, int]:
         self.x = x_teleport
         self.y = y_teleport
+        return self.x, self.y
+
+    def collision_detect(self, target_x: int, target_y: int) -> bool:
+        if self.x == target_x and self.y == target_y:
+            return True
+        return False
 
 
-alien1 = Alien([2, 1], [4, 3])
-alien1.hit()
-print(f"salud Alien: {alien1.salud}")
-print(alien1.is_alive())
-alien1.teleporting([4, 1], [7, 4])
-print(f"{alien1.x} , {alien1.y}")
+def alien_creator(start_positions: list[tuple]) -> list[Alien]:
+    alien_list = [Alien([position[0] for position in start_positions],
+                        [position[1] for position in start_positions])]
+    return alien_list
+
+
+if __name__ == '__main__':
+    try:
+        alien1 = Alien(2, 1)
+        alien2 = Alien(4, 8, 1)
+        try:
+            alien1.hit()
+        except ValueError as e:
+            print(f"No se puede tener 0 o menos vidas, error: {type(e).__name__}")
+        try:
+            alien2.hit()
+        except ValueError as e:
+            print(f"No se puede tener 0 o menos vidas, error: {type(e).__name__}")
+        alien_creator([(4, 7), (-1, 0)])
+        alien_start_positions = [(4, 7), (-1, 0)]
+        aliens = alien_creator(alien_start_positions)
+    except ValueError as e:
+        print(f"No se puede tener 0 o menos vidas {e}")
