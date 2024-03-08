5c86549a99cebd601c5072352127b8991e0c7c8a
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2024-03-08 13:23:01 +0100

    (ejer58) En proceso

--- python/ejer58/ejer58.py
+++ python/ejer58/ejer58.py
@@ -2,6 +2,7 @@ import numpy as np
 
 a = np.array([1, 2, 3, 2, 3, 4, 3, 4, 5, 6])
 b = np.array([7, 2, 10, 2, 7, 4, 9, 4, 9, 8])
+c = np.array([2, 6, 1, 9, 10, 3, 27])
 
 
 def is_equal(arr1: np.ndarray, arr2: np.ndarray) -> list[int]:
@@ -10,7 +11,14 @@ def is_equal(arr1: np.ndarray, arr2: np.ndarray) -> list[int]:
     return equal_numbers
 
 
-is_equal(a, b)
+def found_values(arr: np.ndarray, first: int, second: int) -> list[int]:
+    num_val = [num for num in arr if first <= num <= second]
+    return num_val
+
+
+def vector_arrays(a, b):
+    pass
+
 
 print(is_equal(a, b))
-
+print(found_values(c, 5, 10))
