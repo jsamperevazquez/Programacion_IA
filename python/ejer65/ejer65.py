996f12858840adcf718ca6621912b229ed7374f0
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2024-04-11 19:36:24 +0200

    (ejer65): Implementado el detector de cara

--- python/ejer65/ejer65.py
+++ python/ejer65/ejer65.py
@@ -36,24 +36,43 @@ video_capture.release()
 cv2.destroyAllWindows()
 
 """
-
+import numpy as np
 import cv2
+import dlib
+import time
+
+predictor_path = "shape_predictor_5_face_landmarks.dat"
+face_rec_model_path = "dlib_face_recognition_resnet_model_v1.dat"
+
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor(predictor_path)
+face_rec = dlib.face_recognition_model_v1(face_rec_model_path)
 
 video_capture = cv2.VideoCapture(0)
 
 while True:
     # Captura un frame
     ret, frame = video_capture.read()
-
+    dets = detector(frame, 1)
     # para procesar o frame con outras ferramentas seguramente habería que convertilo a RGB
     # (Lembra que opencv traballa con BGR)
-
+    for d in dets:
+        x, y, w, h = d.left(), d.top(), d.width(), d.height()
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     # Amosa a imaxe resultante
     cv2.imshow('Video', frame)
 
-    # Pulsa 'q' para saír!
+    # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Pulsa 'q' para salir!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    # if len(dets) == 1:
+
+        # print("Number of faces detected: {}".format(len(dets)))
+        # time.sleep(5)
+        # break
 
 # Libera a cámara
 # video_capture.release()
